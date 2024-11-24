4ac5e0abbb699074378ed654971025afbee72328
Kim <fopc4131@gmail.com>
2024-11-25 08:48:30 +0900

    Update Scroll only Currency Cards

--- lib/main.dart
+++ lib/main.dart
@@ -15,142 +15,150 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xFF181818),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 60,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text(
-                          'Hey, Selena',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 26,
-                              fontWeight: FontWeight.w700),
-                        ),
-                        Text(
-                          'Welcome back',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.7),
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Total Balance',
-                      style: TextStyle(
-                        color: Color.fromRGBO(255, 255, 255, 0.8),
-                        fontSize: 20,
-                        fontWeight: FontWeight.w200,
-                      ),
-                    ),
-                    Text(
-                      '\$5 194 392',
-                      style: TextStyle(
-                        color: Color.fromRGBO(255, 255, 255, 1.0),
-                        fontSize: 36,
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        EclipseButton(
-                          text: 'Transfer',
-                          backgroundColor: Color(0xFFF2B33A),
-                          textColor: Color(0xFF181818),
-                        ),
-                        EclipseButton(
-                          text: 'Request',
-                          backgroundColor: Color(0xFF1F2123),
-                          textColor: Colors.white,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 70,
-                ),
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text(
-                          'Wallets',
-                          style: TextStyle(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 60,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Hey, Selena',
+                        style: TextStyle(
                             color: Colors.white,
-                            fontSize: 35,
-                            fontWeight: FontWeight.w700,
-                          ),
+                            fontSize: 26,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.7),
+                          fontSize: 15,
                         ),
-                        Text(
-                          'View All',
-                          style: TextStyle(
-                              color: Colors.white.withOpacity(0.9),
-                              fontSize: 17,
-                              fontWeight: FontWeight.w300),
-                        ),
-                      ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Total Balance',
+                    style: TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 0.8),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w200,
                     ),
-                    const SizedBox(
-                      height: 15,
+                  ),
+                  Text(
+                    '\$5 194 392',
+                    style: TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 1.0),
+                      fontSize: 36,
+                      fontWeight: FontWeight.w800,
                     ),
-                    const CurrencyCard(
-                      unit: 'Euro',
-                      value: '6 428',
-                      code: 'EUR',
-                      icon: Icons.euro_sharp,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      EclipseButton(
+                        text: 'Transfer',
+                        backgroundColor: Color(0xFFF2B33A),
+                        textColor: Color(0xFF181818),
+                      ),
+                      EclipseButton(
+                        text: 'Request',
+                        backgroundColor: Color(0xFF1F2123),
+                        textColor: Colors.white,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    'Wallets',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontWeight: FontWeight.w700,
                     ),
-                    const CurrencyCard(
-                      unit: 'Yenhwa',
-                      value: '6 428',
-                      code: 'YEN',
-                      icon: Icons.currency_yen,
-                      isInvert: true,
-                      offsetY: -20,
+                  ),
+                  Text(
+                    'View All',
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(0.9),
+                        fontSize: 17,
+                        fontWeight: FontWeight.w300),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
                     ),
-                    const CurrencyCard(
-                      unit: 'Bitcoin',
-                      value: '6 428',
-                      code: 'BTC',
-                      icon: Icons.currency_bitcoin,
-                      offsetY: -40,
-                    ),
-                    const CurrencyCard(
-                      unit: 'Rupee',
-                      value: '6 428',
-                      code: 'INR',
-                      icon: Icons.currency_rupee_sharp,
-                      isInvert: true,
-                      offsetY: -60,
-                    ),
-                  ],
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: ListView(
+                    children: const [
+                      CurrencyCard(
+                        unit: 'Euro',
+                        value: '6 428',
+                        code: 'EUR',
+                        icon: Icons.euro_sharp,
+                      ),
+                      CurrencyCard(
+                        unit: 'Yenhwa',
+                        value: '6 428',
+                        code: 'YEN',
+                        icon: Icons.currency_yen,
+                        isInvert: true,
+                        offsetY: -20,
+                      ),
+                      CurrencyCard(
+                        unit: 'Bitcoin',
+                        value: '6 428',
+                        code: 'BTC',
+                        icon: Icons.currency_bitcoin,
+                        offsetY: -40,
+                      ),
+                      CurrencyCard(
+                        unit: 'Rupee',
+                        value: '6 428',
+                        code: 'INR',
+                        icon: Icons.currency_rupee_sharp,
+                        isInvert: true,
+                        offsetY: -60,
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
